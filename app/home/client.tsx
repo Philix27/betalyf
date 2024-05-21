9a55a818b577216c140f882a2bb21a264975b6d4
Philix27 <philixbob@gmail.com>
2024-05-21 10:50:49 +0100

    add next-auth files

--- app/home/client.tsx
+++ app/home/client.tsx
@@ -3,7 +3,6 @@
 import React from "react"
 import { useRouter } from "next/navigation"
 import { Button, TextB, TextH } from "@/comps"
-import { AppPages } from "@/lib"
 import { Github } from "lucide-react"
 
 import FooterSection from "./footer"
